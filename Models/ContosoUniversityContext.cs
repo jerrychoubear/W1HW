f10643332f7538ffee364771671b178e6bfb6b00
JJ <jerrychoubear@gmail.com>
2020-05-21 22:04:50 +0800

    Add dbcontext to services

--- Models/ContosoUniversityContext.cs
+++ Models/ContosoUniversityContext.cs
@@ -25,15 +25,6 @@ namespace W1HW.Models
         public virtual DbSet<VwCourseStudents> VwCourseStudents { get; set; }
         public virtual DbSet<VwDepartmentCourseCount> VwDepartmentCourseCount { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ContosoUniversity;Trusted_Connection=True");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>(entity =>
--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using W1HW.Models;
 
 namespace W1HW
 {
@@ -26,6 +29,8 @@ namespace W1HW
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ContosoUniversityContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
