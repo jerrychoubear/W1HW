3fe31969dcd97efb312519cfd16891b005461b13
JJ <jerrychoubear@gmail.com>
2020-05-21 21:53:56 +0800

    Scaffold dbcontext

---
+++ Models/VwCourseStudentCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace W1HW.Models
+{
+    public partial class VwCourseStudentCount
+    {
+        public int? DepartmentId { get; set; }
+        public string Name { get; set; }
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int? StudentCount { get; set; }
+    }
+}
